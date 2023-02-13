9b66c1e4fe500e63c061228da1a03ea7d41d6e8f
owitn <owitn@KATHERINE_V2>
2023-02-13 22:50:32 +0000

    Add project files.

---
+++ ThingDef_PlagueBullet.cs
@@ -0,0 +1,11 @@
+﻿using RimWorld;
+using Verse;
+
+namespace Gardening
+{
+    public class ThingDef_PlagueBullet : DefModExtension
+    {
+        public float AddHediffChance = 0.5f;
+        public HediffDef HediffToAdd;
+    }
+}
